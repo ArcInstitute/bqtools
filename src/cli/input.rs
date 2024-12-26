395b5a1b9db41dfbe2fbacd22796a5ea84dc9f35
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-26 10:31:44 -0800

    feat: convert encode args to vec input and handle single or paired inputs

--- src/cli/input.rs
+++ src/cli/input.rs
@@ -6,8 +6,8 @@ use crate::commands::match_input;
 
 #[derive(Parser, Debug)]
 pub struct InputFile {
-    #[clap(short, long, help = "Input file [default: stdin]")]
-    pub input: Option<String>,
+    #[clap(help = "Input file [default: stdin]", num_args = 0..=2)]
+    pub input: Vec<String>,
 
     #[clap(short, long, help = "Input file format")]
     pub format: Option<FileFormat>,
@@ -17,24 +17,62 @@ impl InputFile {
         if let Some(format) = self.format {
             Ok(format)
         } else {
-            if let Some(path) = self.input.as_ref() {
-                if let Some(format) = FileFormat::from_path(path) {
-                    Ok(format)
-                } else {
-                    bail!("Could not infer file format.")
+            match self.input.len() {
+                0 => bail!("Can not infer file format from stdin."),
+                _ => {
+                    // Identify file format for each input file
+                    let formats = self
+                        .input
+                        .iter()
+                        .map(|path| {
+                            // Infer file format for each input file
+                            if let Some(format) = FileFormat::from_path(path) {
+                                Ok(format)
+
+                            // Bail if file format could not be inferred
+                            } else {
+                                bail!("Could not infer file format.")
+                            }
+                        })
+                        .collect::<Result<Vec<_>>>()?;
+
+                    // Check if all formats are the same
+                    if formats.iter().all(|&f| f == formats[0]) {
+                        // Return the format
+                        Ok(formats[0])
+                    } else {
+                        // Bail if formats are inconsistent
+                        bail!("Inconsistent file formats.")
+                    }
                 }
-            } else {
-                bail!("Could not infer file format.")
             }
         }
     }
 
     pub fn as_reader(&self) -> Result<Box<dyn Read>> {
-        match_input(self.input.as_ref())
+        match self.input.len() {
+            0 => match_input(None),
+            1 => match_input(Some(&self.input[0])),
+            _ => bail!("Multiple input files are not supported."),
+        }
+    }
+
+    pub fn as_reader_pair(&self) -> Result<(Box<dyn Read>, Box<dyn Read>)> {
+        match self.input.len() {
+            2 => Ok((
+                match_input(Some(&self.input[0]))?,
+                match_input(Some(&self.input[1]))?,
+            )),
+            _ => bail!("Two input files are required."),
+        }
+    }
+
+    pub fn paired(&self) -> bool {
+        self.input.len() == 2
     }
 }
 
-#[derive(ValueEnum, Debug, Clone, Copy)]
+#[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq)]
 pub enum FileFormat {
     Fasta,
     Fastq,
--- src/commands/encode/mod.rs
+++ src/commands/encode/mod.rs
@@ -5,10 +5,10 @@ use std::io::Read;
 
 use crate::{
     cli::{EncodeCommand, FileFormat},
-    process_single_fastx,
+    process_paired_fastx, process_single_fastx,
 };
 
-pub fn run(args: EncodeCommand) -> Result<()> {
+fn encode_single(args: EncodeCommand) -> Result<()> {
     // Open the IO handles
     let in_handle = args.input.as_reader()?;
     let out_handle = args.output.as_writer()?;
@@ -23,5 +23,42 @@ pub fn run(args: EncodeCommand) -> Result<()> {
         FileFormat::Fasta => {
             process_single_fastx!(seq_io::fasta::Reader<Box<dyn Read>>, in_handle, out_handle)
         }
+    }
+}
+
+fn encode_paired(args: EncodeCommand) -> Result<()> {
+    // Open the IO handles
+    let (r1_handle, r2_handle) = args.input.as_reader_pair()?;
+    let out_handle = args.output.as_writer()?;
+
+    // Compression passthrough on input
+    let (r1_handle, _comp) = niffler::get_reader(r1_handle)?;
+    let (r2_handle, _comp) = niffler::get_reader(r2_handle)?;
+
+    match args.input.format()? {
+        FileFormat::Fastq => {
+            process_paired_fastx!(
+                seq_io::fastq::Reader<Box<dyn Read>>,
+                r1_handle,
+                r2_handle,
+                out_handle
+            )
+        }
+        FileFormat::Fasta => {
+            process_paired_fastx!(
+                seq_io::fasta::Reader<Box<dyn Read>>,
+                r1_handle,
+                r2_handle,
+                out_handle
+            )
+        }
+    }
+}
+
+pub fn run(args: EncodeCommand) -> Result<()> {
+    if args.input.paired() {
+        encode_paired(args)
+    } else {
+        encode_single(args)
     }
 }
