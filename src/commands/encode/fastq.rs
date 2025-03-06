9d0ee2d3e29bc0d095b7b1040a6deb01c0c9ca99
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-03-06 11:12:05 -0800

    fix: bug with single encoding with header on vbinseq

--- src/commands/encode/fastq.rs
+++ src/commands/encode/fastq.rs
@@ -49,7 +49,7 @@ pub fn encode_single_fastq_parallel(
             (num_records, num_skipped)
         }
         _ => {
-            let header = VBinseqHeader::with_capacity(block_size as u64, quality, compress, true);
+            let header = VBinseqHeader::with_capacity(block_size as u64, quality, compress, false);
             let processor = VBinseqProcessor::new(header, policy.into(), out_handle)?;
 
             // Process the records in parallel
