dd418f6e5078fc4afd82856afe663343de17e345
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-04-14 11:20:44 -0700

    feat: added routing for interleaved processing and implement interleaved processing on the processor

--- src/commands/encode/utils.rs
+++ src/commands/encode/utils.rs
@@ -26,6 +26,28 @@ pub fn get_sequence_len_fasta(reader: &mut FastaReader) -> Result<u32> {
     Ok(slen as u32)
 }
 
+pub fn get_interleaved_sequence_len_fasta(reader: &mut FastaReader) -> Result<(u32, u32)> {
+    let mut rset = fasta::RecordSet::new(2);
+    let (slen, xlen) = if rset.fill(reader)? {
+        let mut rset_iter = rset.iter();
+        let r1 = if let Some(record) = rset_iter.next() {
+            record?
+        } else {
+            bail!("Input file is empty - cannot convert");
+        };
+        let r2 = if let Some(record) = rset_iter.next() {
+            record?
+        } else {
+            bail!("Input file is empty - cannot convert");
+        };
+        (r1.seq().len(), r2.seq().len())
+    } else {
+        bail!("Input file (interleaved) is missing R2 - cannot convert");
+    };
+    reader.reload(&mut rset);
+    Ok((slen as u32, xlen as u32))
+}
+
 pub fn get_sequence_len_fastq(reader: &mut FastqReader) -> Result<u32> {
     let mut rset = fastq::RecordSet::new(1);
     let slen = if rset.fill(reader)? {
@@ -40,4 +62,26 @@ pub fn get_sequence_len_fastq(reader: &mut FastqReader) -> Result<u32> {
     };
     reader.reload(&mut rset);
     Ok(slen as u32)
+}
+
+pub fn get_interleaved_sequence_len_fastq(reader: &mut FastqReader) -> Result<(u32, u32)> {
+    let mut rset = fastq::RecordSet::new(2);
+    let (slen, xlen) = if rset.fill(reader)? {
+        let mut rset_iter = rset.iter();
+        let r1 = if let Some(record) = rset_iter.next() {
+            record?
+        } else {
+            bail!("Input file is empty - cannot convert");
+        };
+        let r2 = if let Some(record) = rset_iter.next() {
+            record?
+        } else {
+            bail!("Input file (interleaved) is missing R2 - cannot convert");
+        };
+        (r1.seq().len(), r2.seq().len())
+    } else {
+        bail!("Input file is empty - cannot convert");
+    };
+    reader.reload(&mut rset);
+    Ok((slen as u32, xlen as u32))
 }
