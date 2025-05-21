95aa90aed24d0b839685016fafc7bb2373c0179a
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-21 11:28:12 -0700

    tests: added testing for encoding

---
+++ tests/test_encode.rs
@@ -0,0 +1,48 @@
+use std::{path::Path, process::Command};
+
+use anyhow::Result;
+use bon::builder;
+use itertools::iproduct;
+
+mod common;
+use common::{
+    output_tempfile, write_fastx, BinseqMode, CompressionStatus, FastxFormat, COMMAND_NAME,
+};
+
+#[builder]
+fn run_encode(in_path: &Path, out_path: &Path, threads: Option<usize>) -> Result<bool> {
+    let mut args: Vec<_> = vec![
+        "encode",
+        in_path.to_str().unwrap(),
+        "-o",
+        out_path.to_str().unwrap(),
+    ]
+    .into_iter()
+    .map(|x| x.to_string())
+    .collect();
+    if let Some(t) = threads {
+        args.push("-t".to_string());
+        args.push(format!("{}", t));
+    }
+    let cmd = Command::new(COMMAND_NAME).args(args).output()?;
+    Ok(cmd.status.success())
+}
+
+#[test]
+fn test_encoding() -> Result<()> {
+    for (mode, comp, format) in iproduct!(
+        BinseqMode::enum_iter(),
+        CompressionStatus::enum_iter(),
+        FastxFormat::enum_iter(),
+    ) {
+        let in_tmp = write_fastx().format(format).comp(comp).call()?;
+        let out_tmp = output_tempfile(mode)?;
+        eprintln!("Testing: {:?} {:?} {:?}", mode, comp, format);
+        let status = run_encode()
+            .in_path(in_tmp.path())
+            .out_path(out_tmp.path())
+            .call()?;
+        assert!(status);
+    }
+    Ok(())
+}
