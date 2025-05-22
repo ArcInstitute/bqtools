f8124bd4e5cf7b7825ab65d2911ddad6f68651b8
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-22 10:23:51 -0700

    feat: added counting option for vbq

--- src/commands/count/mod.rs
+++ src/commands/count/mod.rs
@@ -1,26 +1,36 @@
 use anyhow::Result;
-use binseq::bq::MmapReader;
+use binseq::{bq::MmapReader, vbq, BinseqReader};
 
 use crate::cli::CountCommand;
 
-fn log_reader(reader: &MmapReader, only_num: bool) {
+fn log_reader_bq(reader: &MmapReader, num_records: usize) {
     let header = reader.header();
-    let num_records = reader.num_records();
-
-    if only_num {
-        println!("{num_records}");
-    } else {
-        println!("Format Version    : {}", header.format);
-        println!("Sequence Length   : {}", header.slen);
-        if header.xlen > 0 {
-            println!("Extended Length   : {}", header.xlen);
-        }
-        println!("Number of records : {num_records}");
+    println!("Format Version    : {}", header.format);
+    println!("Sequence Length   : {}", header.slen);
+    if header.xlen > 0 {
+        println!("Extended Length   : {}", header.xlen);
     }
+    println!("Number of records : {num_records}");
+}
+
+fn log_reader_vbq(reader: &vbq::MmapReader, num_records: usize) {
+    let header = reader.header();
+    println!("Format Version    : {}", header.format);
+    println!("Compression:      : {}", header.compressed);
+    println!("Quality:          : {}", header.qual);
+    println!("Number of records : {num_records}");
 }
 
 pub fn run(args: &CountCommand) -> Result<()> {
-    let reader = MmapReader::new(args.input.path())?;
-    log_reader(&reader, args.opts.num);
+    let reader = BinseqReader::new(args.input.path())?;
+    let num_records = reader.num_records()?;
+    if args.opts.num {
+        println!("{num_records}");
+    } else {
+        match reader {
+            BinseqReader::Bq(ref bq_reader) => log_reader_bq(bq_reader, num_records),
+            BinseqReader::Vbq(ref vbq_reader) => log_reader_vbq(vbq_reader, num_records),
+        }
+    }
     Ok(())
 }
