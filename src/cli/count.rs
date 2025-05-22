c2d5c306a9e1f9be1563cfa84eed4945aec5e490
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-22 09:39:58 -0700

    feat: added flag on count

--- src/cli/count.rs
+++ src/cli/count.rs
@@ -7,4 +7,15 @@ use super::InputBinseq;
 pub struct CountCommand {
     #[clap(flatten)]
     pub input: InputBinseq,
+
+    #[clap(flatten)]
+    pub opts: CountOpts,
+}
+
+#[derive(Parser, Debug)]
+#[clap(next_help_heading = "COUNT OPTIONS")]
+pub struct CountOpts {
+    /// Only print the number of records in the file
+    #[clap(short, long)]
+    pub num: bool,
 }
