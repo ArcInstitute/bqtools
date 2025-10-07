70005826e576bd6f8debb017a391564bd30dca99
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-10-07 11:05:51 -0700

    feat: auto determine output name using same logic as recursive naming

--- src/cli/encode.rs
+++ src/cli/encode.rs
@@ -1,3 +1,8 @@
+use anyhow::{bail, Result};
+use log::{error, trace};
+
+use crate::commands::encode::utils::generate_output_name;
+
 use super::{BinseqMode, InputFile, OutputBinseq};
 
 #[derive(clap::Parser, Debug, Clone)]
@@ -10,13 +15,33 @@ pub struct EncodeCommand {
     pub output: OutputBinseq,
 }
 impl EncodeCommand {
-    pub fn mode(&self) -> anyhow::Result<BinseqMode> {
+    pub fn mode(&self) -> Result<BinseqMode> {
         if let Some(mode) = self.output.mode {
             Ok(mode)
         } else if self.input.recursive {
             Ok(BinseqMode::VBinseq)
         } else {
             self.output.mode()
+        }
+    }
+    pub fn output_path(&self) -> Result<Option<String>> {
+        if let Some(path) = &self.output.output {
+            Ok(Some(path.to_string()))
+        } else if self.output.pipe {
+            Ok(None)
+        } else if self.input.is_stdin() {
+            error!("Output path must be provided if using stdin");
+            bail!("Output path must be provided if using stdin")
+        } else {
+            let outpath = if self.input.paired() {
+                let (r1, r2) = self.input.paired_paths()?;
+                generate_output_name(&[r1.into(), r2.into()], self.mode()?.extension())?
+            } else {
+                let path = self.input.single_path()?.unwrap();
+                generate_output_name(&[path.into()], self.mode()?.extension())?
+            };
+            trace!("Auto-determined outpath path: {outpath}");
+            Ok(Some(outpath))
         }
     }
 }
