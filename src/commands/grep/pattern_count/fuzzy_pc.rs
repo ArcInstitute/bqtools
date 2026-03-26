a25fa96cf4f64db3b56794d08c6316f823f58012
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2026-03-26 13:12:00 -0700

    refactor: move to multiple pattern searching

--- src/commands/grep/pattern_count/fuzzy_pc.rs
+++ src/commands/grep/pattern_count/fuzzy_pc.rs
@@ -1,20 +1,27 @@
 use super::{PatternCollection, PatternCount};
 
-use sassy::{profiles::Iupac, Searcher};
+use sassy::{profiles::Iupac, EncodedPatterns, Match, Searcher};
 
-type Patterns = Vec<Vec<u8>>;
+type Profile = Iupac;
+
 #[derive(Clone)]
 pub struct FuzzyPatternCounter {
     /// Patterns to fuzzy match on
-    pat1: Patterns, // in primary
-    pat2: Patterns, // in secondary
-    pat: Patterns,  // in either
-    k: usize,       // maximum edit distance to accept
-    inexact: bool,  // whether to only report inexact matches
-    invert: bool,   // invert the match
+    pat1: Option<EncodedPatterns<Profile>>, // in primary
+    pat2: Option<EncodedPatterns<Profile>>, // in secondary
+    pat: Option<EncodedPatterns<Profile>>,  // in either
+    k: usize,                               // maximum edit distance to accept
+    inexact: bool,                          // whether to only report inexact matches
+    invert: bool,                           // invert the match
 
     all_patterns: PatternCollection,
-    searcher: Searcher<Iupac>,
+
+    /// Primary sequence searcher
+    searcher_1: Searcher<Profile>,
+    /// Extended sequence searcher
+    searcher_2: Searcher<Profile>,
+    /// Combined searcher
+    searcher: Searcher<Profile>,
 }
 
 impl FuzzyPatternCounter {
@@ -26,70 +33,92 @@ impl FuzzyPatternCounter {
         inexact: bool,
         invert: bool,
     ) -> Self {
-        let pat1_bytes = pat1.bytes();
-        let pat2_bytes = pat2.bytes();
-        let pat_bytes = pat.bytes();
+        // initialize a searcher for each pattern collection
+        let mut searcher_1 = Searcher::new_fwd();
+        let mut searcher_2 = Searcher::new_fwd();
+        let mut searcher = Searcher::new_fwd();
+
+        // encode the patterns for each collection/searcher combination
+        let enc_pat1 = pat1
+            .has_patterns()
+            .then_some(searcher_1.encode_patterns(&pat1.bytes()));
+        let enc_pat2 = pat2
+            .has_patterns()
+            .then_some(searcher_2.encode_patterns(&pat2.bytes()));
+        let enc_pat = pat
+            .has_patterns()
+            .then_some(searcher.encode_patterns(&pat.bytes()));
+
+        // combine all patterns into a single collection for reporting
         let all_patterns = PatternCollection(pat1.into_iter().chain(pat2).chain(pat).collect());
+
         Self {
-            pat1: pat1_bytes,
-            pat2: pat2_bytes,
-            pat: pat_bytes,
+            pat1: enc_pat1,
+            pat2: enc_pat2,
+            pat: enc_pat,
             k,
             inexact,
             invert,
             all_patterns,
-            searcher: Searcher::new_fwd(),
+            searcher_1,
+            searcher_2,
+            searcher,
         }
     }
 
     fn match_primary(&mut self, sequence: &[u8], pattern_counts: &mut [usize]) {
-        if self.pat1.is_empty() {
-            return;
-        }
-        self.pat1.iter().enumerate().for_each(|(index, pat)| {
-            let counted = self
-                .searcher
-                .search(pat, &sequence, self.k)
+        if let Some(ref epat) = self.pat1 {
+            self.searcher_1
+                .search_all_encoded_patterns(epat, sequence, self.k)
                 .iter()
-                .any(|mat| !self.inexact || mat.cost != 0);
-            if counted != self.invert {
-                pattern_counts[index] += 1;
-            }
-        });
+                .for_each(|m| {
+                    let counted = !self.inexact || m.cost != 0;
+                    if counted != self.invert {
+                        pattern_counts[m.pattern_idx] += 1;
+                    }
+                });
+        }
     }
 
     fn match_secondary(&mut self, sequence: &[u8], pattern_counts: &mut [usize]) {
-        if self.pat2.is_empty() || sequence.is_empty() {
-            return;
-        }
-        self.pat2.iter().enumerate().for_each(|(index, pat)| {
-            let counted = self
-                .searcher
-                .search(pat, &sequence, self.k)
+        if let Some(ref epat) = self.pat2 {
+            let offset = self.pat1.as_ref().map_or(0, |p| p.n_queries());
+            self.searcher_2
+                .search_all_encoded_patterns(epat, sequence, self.k)
                 .iter()
-                .any(|mat| !self.inexact || mat.cost != 0);
-            if counted != self.invert {
-                pattern_counts[self.pat1.len() + index] += 1;
-            }
-        });
+                .for_each(|m| {
+                    let counted = !self.inexact || m.cost != 0;
+                    if counted != self.invert {
+                        pattern_counts[offset + m.pattern_idx] += 1;
+                    }
+                });
+        }
     }
 
     fn match_either(&mut self, primary: &[u8], secondary: &[u8], pattern_counts: &mut [usize]) {
-        if self.pat.is_empty() {
-            return;
-        }
-        self.pat.iter().enumerate().for_each(|(index, pat)| {
-            let counted = self
-                .searcher
-                .search(pat, &primary, self.k)
-                .iter()
-                .chain(self.searcher.search(pat, &secondary, self.k).iter())
-                .any(|mat| !self.inexact || mat.cost != 0);
+        if let Some(ref epat) = self.pat {
+            let offset = self.pat1.as_ref().map_or(0, |p| p.n_queries())
+                + self.pat2.as_ref().map_or(0, |p| p.n_queries());
 
-            if counted != self.invert {
-                pattern_counts[self.pat1.len() + self.pat2.len() + index] += 1;
-            }
-        });
+            let eval = |m: &Match, pattern_counts: &mut [usize]| {
+                let counted = !self.inexact || m.cost != 0;
+                if counted != self.invert {
+                    pattern_counts[offset + m.pattern_idx] += 1;
+                }
+            };
+
+            // match on primary
+            self.searcher
+                .search_all_encoded_patterns(epat, primary, self.k)
+                .iter()
+                .for_each(|m| eval(m, pattern_counts));
+
+            // match on secondary
+            self.searcher
+                .search_all_encoded_patterns(epat, secondary, self.k)
+                .iter()
+                .for_each(|m| eval(m, pattern_counts));
+        }
     }
 }
 
@@ -101,7 +130,9 @@ impl PatternCount for FuzzyPatternCounter {
     }
 
     fn num_patterns(&self) -> usize {
-        self.pat1.len() + self.pat2.len() + self.pat.len()
+        self.pat1.as_ref().map_or(0, |p| p.n_queries())
+            + self.pat2.as_ref().map_or(0, |p| p.n_queries())
+            + self.pat.as_ref().map_or(0, |p| p.n_queries())
     }
 
     fn pattern_strings(&self) -> Vec<String> {
