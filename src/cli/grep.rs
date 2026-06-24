6d5003e1d122a76049ad5cd1e8ce8ab33111a4b3
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2026-06-24 10:40:17 -0700

    fix: correct required unless present any flags

--- src/cli/grep.rs
+++ src/cli/grep.rs
@@ -201,7 +201,7 @@ pub struct PatternFileArgs {
     /// (sequences are used as patterns), or TSV (alias / pattern).
     /// FASTA files and TSVs are auto-detected.
     /// Patterns may be regex or literal (fuzzy doesn't support regex).
-    #[clap(long, required_unless_present_any = ["sfile", "xfile"])]
+    #[clap(long, required_unless_present_any = ["reg1", "reg2", "reg", "sfile", "xfile"])]
     pub file: Option<String>,
 
     /// File of patterns to search for in primary sequence
@@ -210,7 +210,7 @@ pub struct PatternFileArgs {
     /// (sequences are used as patterns), or TSV (alias / pattern).
     /// FASTA files and TSVs are auto-detected.
     /// Patterns may be regex or literal (fuzzy doesn't support regex).
-    #[clap(long, required_unless_present_any = ["file", "xfile"])]
+    #[clap(long, required_unless_present_any = ["reg1", "reg2", "reg", "file", "xfile"])]
     pub sfile: Option<String>,
 
     /// File of patterns to search for in extended sequence
@@ -219,7 +219,7 @@ pub struct PatternFileArgs {
     /// (sequences are used as patterns), or TSV (alias / pattern).
     /// FASTA files and TSVs are auto-detected.
     /// Patterns may be regex or literal (fuzzy doesn't support regex).
-    #[clap(long, required_unless_present_any = ["file", "sfile"])]
+    #[clap(long, required_unless_present_any = ["reg1", "reg2", "reg", "file", "sfile"])]
     pub xfile: Option<String>,
 }
 
